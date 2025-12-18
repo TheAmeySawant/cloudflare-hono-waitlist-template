8fd0f2ebee45ae46ba51fc27baa9ef7d58486621
Amey Sawant <amey01962@gmail.com>
2025-12-18 19:29:40 +0530

    Local Cloudflare Server Setup

---
+++ src/client/components/ServerStatus.tsx
@@ -0,0 +1,20 @@
+import { useEffect , useState} from "react"
+
+
+export const ServerStatus = () => {
+
+    const [status, setStatus] = useState('')
+
+    useEffect(() => {
+        fetch('/api/health')
+        .then(res => res.json())
+        .then(data => setStatus(data))
+    }, [])
+
+    return (
+        <div>
+            <h1>Server Status</h1>
+            <p>{status}</p>
+        </div>
+    )
+}
